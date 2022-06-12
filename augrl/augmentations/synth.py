fixed scaling on augmentations

augrl/augmentations/synth.py
@@ -9,13 +9,13 @@ from d3rlpy.dataset import Transition
 
 
 def generate_new_data(
-    transitions: List[Transition], fn: Callable, args: Dict
+    transitions: List[Transition], fn: Callable, scaling: np.ndarray, args: Dict
 ) -> List[Transition]:
-    return [fn(trans, **args) for trans in transitions]
+    return [fn(trans, scaling, **args) for trans in transitions]
 
 
-def gaussian(trans: Transition, sigma: float = 1e-3) -> Transition:
-    augmented_obs = trans.observation + np.random.normal(
+def gaussian(trans: Transition, scaling: np.ndarray, sigma: float = 1e-3) -> Transition:
+    augmented_obs = trans.observation + scaling * np.random.normal(
         loc=0, scale=sigma, size=trans.get_observation_shape()
     )
     return Transition(
@@ -29,8 +29,8 @@ def gaussian(trans: Transition, sigma: float = 1e-3) -> Transition:
     )
 
 
-def uniform(trans: Transition, alpha: float = 1e-3) -> Transition:
-    augmented_obs = trans.observation + np.random.uniform(
+def uniform(trans: Transition, scaling: np.ndarray, alpha: float = 1e-3) -> Transition:
+    augmented_obs = trans.observation + scaling * np.random.uniform(
         loc=-alpha, scale=alpha, size=trans.get_observation_shape()
     )
     return Transition(
@@ -44,7 +44,9 @@ def uniform(trans: Transition, alpha: float = 1e-3) -> Transition:
     )
 
 
-def mixup(trans: Transition, eps: float = 0.4) -> Transition:
+def mixup(trans: Transition, scaling: np.ndarray, eps: float = 0.4) -> Transition:
+    # scaling not needed for mixup
+    _ = scaling
     gamma = np.random.beta(eps, eps, size=trans.get_observation_shape())
     # s_t = gamma * s_t + (1 - gamma) * s_{t+1}
     augmented_obs = gamma * trans.observation * (1 - gamma) * trans.next_observation

augrl/utils.py
@@ -1,7 +1,8 @@
-from typing import Tuple
+from typing import Union
 
 import numpy as np
 from d3rlpy.dataset import MDPDataset
+from d3rlpy.preprocessing.scalers import Scaler
 
 
 def trim(dataset: MDPDataset, ratio: float) -> MDPDataset:
@@ -21,19 +22,13 @@ def trim(dataset: MDPDataset, ratio: float) -> MDPDataset:
     return dataset
 
 
-def normalize(dataset: MDPDataset) -> Tuple[MDPDataset, np.ndarray, np.ndarray]:
-    min_obs = dataset.observations.min(0)
-    max_obs = dataset.observations.max(0)
-    ptp_obs = max_obs - min_obs
-    norm_observations = (dataset.observations - min_obs) / ptp_obs
-    return (
-        MDPDataset(
-            norm_observations,
-            dataset.actions,
-            dataset.rewards,
-            dataset.terminals,
-            dataset.episode_terminals,
-        ),
-        min_obs,
-        max_obs,
-    )
+def get_scaling_factor(scaler: Scaler) -> Union[np.ndarray, float]:
+    if scaler is None:
+        return 1.0
+    if scaler.get_type() == "min_max":
+        params = scaler.get_params()
+        return params["maximum"] - params["minimum"]
+    if scaler.get_type() == "standard":
+        params = scaler.get_params()
+        return params["std"]
+    return 1.0